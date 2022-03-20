8d8de9fb659ef8c11383cb5e4ac158a4f503484c
BIack1st <steveminecrap69@gmail.com>
2022-03-19 19:08:38 -0500

    s

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-var express = require('express');
-var ffmpeg = require("ffmpeg");
-var res = require('express/lib/response');
-var app = express();
-app.get('/', function (req, res) {
-    res.send('f2343');
-});
-
+const jimp = require('jimp')
+const express = require('express')
+const app = express()
+app.get('/api/img', async (req, res) => {
+    let url = req.query.url
+    console.log(url)
+       jimp.read(url, async function (err, image) {
+           let data = [{'dimensions': {'width': 0, 'height': 0}, 'colors': []}]
+           data[0].dimensions.width = image.getWidth()
+           data[0].dimensions.height = image.getHeight()
+            for (let x = 0; x < image.getWidth(); x++) {
+                for (let y = 0; y < image.getHeight(); y++) {
+                    let colors = data[0].colors
+                    let hex = await image.getPixelColor(x,y); 
+                    let rgb = await jimp.intToRGBA(hex);
+                    let dataArray = {'r': rgb['r'],'b': rgb['b'],'g': rgb['g'], 'x': x, 'y':y}
+                    colors.push(dataArray)
+                }
+            }
+            res.json(data) 
+    });
     
-app.get('/api/video-extraction', function (req, res) {
-    var params = req.params;
-    var query = req.query;
-    // params
-    var url = query.url;
-    // query
-    var width = query.width;
-    var height = query.height;
-    var frames = getFrames(url);
-    res.send(url);
-});
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-    console.log("listening on ".concat(port));
-});
+    
+})
+
+
+app.listen(3000, () => {
+    console.log('listening')
+})
